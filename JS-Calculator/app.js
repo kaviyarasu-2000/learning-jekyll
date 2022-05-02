24e13a90208dfe4bdd35c7abca4b13c62722f103
kaviyarasu2001 <kaviyarasu@mallow-tech.com>
2022-05-02 08:55:11 +0530

    function writing

--- JS-Calculator/app.js
+++ JS-Calculator/app.js
@@ -1,59 +1,84 @@
-    function button(val)
-         {
-             document.getElementById("solution").value+=val;
-         }
-         function answer(s) {
-         const r = [];
-         let number = " ";
-         for (const character of s) {
-             if ('%*/+-'.indexOf(character) > -1) {
-                 if (number === '' && character === '-') {
-                     number = '-';
-                 } else {
-                     r.push(parseFloat(number), character);
-                     number = "";
-                 }
-             } else {
-                 number += character;
-             }
-         }
-         if (number !== '') {
-             r.push(parseFloat(number));
-         }
-         return r;
-         }
-         
-         function calculate(numbers) {
-         const operatorPrecedence = [{'%': (a, b) => a % b},{'*': (a, b) => a * b, '/': (a, b) => a / b},
-                                     {'+': (a, b) => a + b, '-': (a, b) => a - b}];
-         let operator;
-         for (const operators of operatorPrecedence) {
-             const newNumbers = [];
-             for (const number of numbers) {
-                 if (number in operators) {
-                     operator = operators[number];
-                 } else if (operator) {
-                     newNumbers[newNumbers.length - 1] = 
-                         operator(newNumbers[newNumbers.length - 1], number);
-                       operator = null;
-                 } else {
-                     newNumbers.push(number);
-                 }
-             }
-             numbers = newNumbers;
-         }
-         if (numbers.length > 1) {
+function button(value) {
+    document.getElementById("solution").value += value;
+}
+   
+function result(s) {
+    const r = [];
+    let number = '';
+    for (const char of s) {
+        if ('%*/+-'.indexOf(char) > -1) {
+            if (number === '' && char === '-') {
+                number = '-';
+            } else {
+                r.push(parseFloat(number), char);
+                number = "";
+            }
+        } else {
+            number += char;
+        }
+    }
+    if (number !== '') {
+        r.push(parseFloat(number));
+    }
+    return r;
+}
 
-             return numbers;
-         } else {
-        
-             document.getElementById('solution').value = numbers[0];
-         }
-         }
-         function solve(){
-         let a = document.getElementById('solution').value;
-             calculate(answer(a));
-         }
-         function clr(){
-             document.getElementById("solution").value="";
-         }
+function calculate(numbers) {
+    const operatorPrecedence = [{
+            '%': function(a, b) {
+                return a % b
+            }
+        },
+        {
+            '*': function(a, b) {
+                return a * b
+            },
+            'x': function(a, b) {
+                return a * b
+            },
+            '/': function(a, b) {
+                return a / b
+            },
+        },
+        {
+            '+': function(a, b) {
+                return a + b
+            },
+            '-': function(a, b) {
+                return a - b
+            }
+        }
+    ];
+    let operator;
+    for (const operators of operatorPrecedence) {
+        const newNumbers = [];
+        for (const number of numbers) {
+            if (number in operators) {
+                operator = operators[number];
+            } else if (operator) {
+                newNumbers[newNumbers.length - 1] =
+                    operator(newNumbers[newNumbers.length - 1], number);
+                operator = null;
+            } else {
+                newNumbers.push(number);
+            }
+        }
+        numbers = newNumbers;
+    }
+    if (numbers.length > 1) {
+
+        return numbers;
+    } else {
+
+        document.getElementById('solution').value = numbers[0];
+    }
+}
+
+function solve() {
+    let a = document.getElementById('solution').value;
+    calculate(result(a));
+}
+
+function clr() {
+    document.getElementById("solution").value = " ";
+};
