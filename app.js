db16cf3992e76c34642629aaeb8e8c17efa544e5
kaviyarasu2001 <kaviyarasu@mallow-tech.com>
2022-05-20 11:36:52 +0530

    function change.

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 $(document).ready(function() {
+    var textColor = 1;
 	$('.font').on('click', function() { 
 		var textColor = $(this).val();
+        color=$(this).val();
 		$('.textshow').css("color", textColor);
 		$('#showtextcolor').val(textColor);
 		$('.colortxt').css("background-color", textColor);
 	});
 	$('.container').on('click', function() { 
-		var containerColor = $(this).val(); 
+        	var containerColor = $(this).val(); 
+		if(containerColor == color) {
+			$(color).attr("disabled", 'disabled');
+		} else {
+			$(color).attr("enable", 'enable');
+	
         $('.showbox').css("background-color", containerColor);
         $('#Bg-color').val(containerColor);
         $('.boxcolor').css("background-color", containerColor);
-		
+        }
 	});
        $('#showtext').keyup(function() {
        let write = $(this).val();
@@ -19,8 +26,25 @@ $(document).ready(function() {
     })
 });
 
-      function myFunction(id) { 
-      var popup = document.getElementById(id);
-      popup.classList.toggle("show");
-}
-    
+      function colorPopup(id) {
+            var pop = document.getElementById(id);
+            var popup = document.getElementById("Popup");
+            var mypopup = document.getElementById("mypopup");
+            if(id == "Popup" && mypopup.classList.contains("show")){
+                 mypopup.classList.toggle("show");
+                }else if(id == "mypopup" && popup.classList.contains("show")){
+                 popup.classList.toggle("show");
+                }
+                 pop.classList.toggle("show");
+                }
+
+
+
+
+
+
+
+
+
+
+
