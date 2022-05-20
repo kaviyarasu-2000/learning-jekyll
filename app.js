c272983894efb678592433da75202ca2641cc4e5
kaviyarasu2001 <kaviyarasu@mallow-tech.com>
2022-05-20 16:23:52 +0530

    text overflow control.

--- app.js
+++ app.js
@@ -1,50 +1,39 @@
 $(document).ready(function() {
     var textColor = 1;
-	$('.font').on('click', function() { 
-		var textColor = $(this).val();
-        color=$(this).val();
-		$('.textshow').css("color", textColor);
-		$('#showtextcolor').val(textColor);
-		$('.colortxt').css("background-color", textColor);
-	});
-	$('.container').on('click', function() { 
-        	var containerColor = $(this).val(); 
-		if(containerColor == color) {
-			$(color).attr("disabled", 'disabled');
-		} else {
-			$(color).attr("enable", 'enable');
-	
-        $('.showbox').css("background-color", containerColor);
-        $('#Bg-color').val(containerColor);
-        $('.boxcolor').css("background-color", containerColor);
+    $('.font').on('click', function() {
+        var textColor = $(this).val();
+        color = $(this).val();
+        $('.textshow').css("color", textColor);
+        $('#showtextcolor').val(textColor);
+        $('.colortxt').css("background-color", textColor);
+    });
+    $('.container').on('click', function() {
+        var containerColor = $(this).val();
+        if (containerColor == color) {
+            $(color).attr("disabled", 'disabled');
+        } else {
+            $(color).attr("enable", 'enable');
+
+            $('.showbox').css("background-color", containerColor);
+            $('#Bg-color').val(containerColor);
+            $('.boxcolor').css("background-color", containerColor);
         }
-	});
-       $('#showtext').keyup(function() {
-       let write = $(this).val();
-       $('.textshow').text(write);
-        
+    });
+    $('#showtext').keyup(function() {
+        let write = $(this).val();
+        $('.textshow').text(write);
+
     })
 });
 
-      function colorPopup(id) {
-            var pop = document.getElementById(id);
-            var popup = document.getElementById("Popup");
-            var mypopup = document.getElementById("mypopup");
-            if(id == "Popup" && mypopup.classList.contains("show")){
-                 mypopup.classList.toggle("show");
-                }else if(id == "mypopup" && popup.classList.contains("show")){
-                 popup.classList.toggle("show");
-                }
-                 pop.classList.toggle("show");
-                }
-
-
-
-
-
-
-
-
-
-
-
+function colorPopup(id) {
+    var pop = document.getElementById(id);
+    var popup = document.getElementById("Popup");
+    var mypopup = document.getElementById("mypopup");
+    if (id == "Popup" && mypopup.classList.contains("show")) {
+        mypopup.classList.toggle("show");
+    } else if (id == "mypopup" && popup.classList.contains("show")) {
+        popup.classList.toggle("show");
+    }
+    pop.classList.toggle("show");
+}
